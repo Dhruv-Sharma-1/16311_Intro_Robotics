042b42825f7ca90769cbdb42676cde7f99687544
Dhruv Sharma <dhruvsharma1999@gmail.com>
2020-08-20 21:02:59 -0400

    clear

--- lab10/my_first_simulation/controllers/epuck_avoid_collision/build/release/epuck_avoid_collision.d
+++
@@ -1,6 +0,0 @@
-build/release/epuck_avoid_collision.o: epuck_avoid_collision.c \
-  /Applications/Webots.app/include/controller/c/webots/robot.h \
-  /Applications/Webots.app/include/controller/c/webots/types.h \
-  /Applications/Webots.app/include/controller/c/webots/nodes.h \
-  /Applications/Webots.app/include/controller/c/webots/motor.h \
-  /Applications/Webots.app/include/controller/c/webots/distance_sensor.h
